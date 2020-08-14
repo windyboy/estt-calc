da12e0c02931b3be5b01f289c1ac4aae39e88df5
zhiqiang feng <fengzhq@it2000.com.cn>
2020-08-14 17:01:32 +0800

    use data class

--- src/main/kotlin/com/gzzn/airport/model/FlyingTimeResponse.kt
+++ src/main/kotlin/com/gzzn/airport/model/FlyingTimeResponse.kt
@@ -2,12 +2,11 @@ package com.gzzn.airport.model
 
 import java.util.*
 
-class FlyingTimeResponse(
-	val flightNumber : String,
-	val flightDate : Date,
-	val flyingTime : Int,
-	val history : Boolean,
-	val seasonal : Boolean,
-	val message : String
-) {
-}
+data class FlyingTimeResponse(
+	val flightNumber: String,
+	val flightDate: Date,
+	val flyingTime: Int,
+	val history: Boolean,
+	val seasonal: Boolean,
+	val message: String
+)
