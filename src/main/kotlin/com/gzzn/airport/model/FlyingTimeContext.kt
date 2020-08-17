5be717529fd134ee853d9c81b7f8534183fcdfc7
zhiqiang feng <fengzhq@it2000.com.cn>
2020-08-17 10:14:22 +0800

    remove unused properties

--- src/main/kotlin/com/gzzn/airport/model/FlyingTimeContext.kt
+++ src/main/kotlin/com/gzzn/airport/model/FlyingTimeContext.kt
@@ -1,12 +1,7 @@
 package com.gzzn.airport.model
 
-import java.util.*
 
 class FlyingTimeContext(
-	val flightNumber: String,
-	val flightDate : Date,
-	val flightSeason: FlightSeason?,
 	val seasonalFlight : SeasonalFlight?,
 	val historyFlights: List<HistoryFlight>
-) {
-}
+)
