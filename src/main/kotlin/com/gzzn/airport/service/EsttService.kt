439d1c822f316a2d51b0eac8ff9bb564da5e4d0b
zhiqiang feng <fengzhq@it2000.com.cn>
2020-08-31 17:36:14 +0800

    add history filter log

--- src/main/kotlin/com/gzzn/airport/service/EsttService.kt
+++ src/main/kotlin/com/gzzn/airport/service/EsttService.kt
@@ -135,15 +135,12 @@ class EsttService(
 	private fun getQualifiedHistoryFlights(flyingTimeContext: FlyingTimeContext): List<HistoryFlight> {
 		return flyingTimeContext.historyFlights.asSequence()
 			.sortedByDescending { it.scheduledTime }
-			.filter { checkFlightTooMuchDelay(it, flyingTimeContext.seasonalFlight) }
+			.filter { checkFlightTooMuchDelay(it) }
 			.toList()
 	}
 
-	private fun checkFlightTooMuchDelay(historyFlight: HistoryFlight, seasonalFlight: SeasonalFlight?): Boolean {
+	private fun checkFlightTooMuchDelay(historyFlight: HistoryFlight): Boolean {
 		val minutes = getMinutes(historyFlight.scheduledTime, historyFlight.actualTime)
-		if (seasonalFlight != null) {
-			return abs(seasonalFlight.flyingTime - minutes) < minHistoryFlight && minutes < maxHistoryDelay
-		}
 		return minutes < maxHistoryDelay
 	}
 
