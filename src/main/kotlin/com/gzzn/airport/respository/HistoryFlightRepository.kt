522b0a83697cea62dd74b83e7c7d9301756171ff
zhiqiang feng <fengzhq@it2000.com.cn>
2020-08-31 15:50:46 +0800

    add actual date time not null

--- src/main/kotlin/com/gzzn/airport/respository/HistoryFlightRepository.kt
+++ src/main/kotlin/com/gzzn/airport/respository/HistoryFlightRepository.kt
@@ -8,6 +8,6 @@ import java.util.*
 
 @JdbcRepository(dialect = Dialect.ORACLE)
 interface HistoryFlightRepository {
-	@Query("select PRE_DEPT_DATETIME_ACTUAL pre_actual_time , ACTUAL_DATETIME actual_time, flight_date, SCHEDULED_DATETIME scheduled_time from FIMS_FLIGHTSCHD_HST historyflight where ARRI_OR_DEPT='A' and flight_number = :flightNumber and flight_date between  :startDate and :endDate order by flight_date desc")
+	@Query("select PRE_DEPT_DATETIME_ACTUAL pre_actual_time , ACTUAL_DATETIME actual_time, flight_date, SCHEDULED_DATETIME scheduled_time from FIMS_FLIGHTSCHD_HST historyflight where ACTUAL_DATETIME is not null and  ARRI_OR_DEPT='A' and flight_number = :flightNumber and flight_date between  :startDate and :endDate order by flight_date desc")
 	fun getArrivalFlight(flightNumber: String, startDate: Date, endDate: Date): List<HistoryFlight>
 }
