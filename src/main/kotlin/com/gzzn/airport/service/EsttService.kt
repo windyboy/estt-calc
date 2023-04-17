7bb6ebbf295bd22a210000a86da6ca00bc224226
zhiqiang feng <fengzhq@it2000.com.cn>
2023-04-17 10:48:19 +0800

    update javadoc

--- src/main/kotlin/com/gzzn/airport/service/EsttService.kt
+++ src/main/kotlin/com/gzzn/airport/service/EsttService.kt
@@ -101,19 +101,26 @@ class EsttService(
 
 
     /**
-     * calculate the flying time of the departure flight with flight number and flight date
+     * 根据给定的航班号和日期计算飞行时间。飞行时间是根据历史航班和季节性航班数据计算的。
      *
-     * 1. get seasonal flight with flight number and flight date
-     * 1.1 if no seasonal flight (seasonal plan not active ?), return (fly-time=0)
-     * 2. get flight history of this seasonal flight
-     * 3. get qualified history flights
-     * 4. if there is enough historical flight, calculate the average fly-time ,return
-     * 4.1 return seasonal flight fly-time
+     * @param flightNumber 表示航班号的字符串。
+     * @param flightDate 表示航班日期的 Date 对象。
+     * @return FlyingTimeResponse 对象，包含以下属性：
+     *          - flightNumber：航班号，String 类型。
+     *          - flightDate：航班日期，Date 类型。
+     *          - flyingTime：计算出的飞行时间，Int 类型。
+     *          - history：一个布尔值，表示飞行时间是否使用历史航班数据计算。
+     *          - seasonal：一个布尔值，表示飞行时间是否使用季节性航班数据计算。
+     *          - message：一个字符串，包含关于如何计算飞行时间的描述性消息。
      *
-     * @param flightNumber flight number
-     * @param flightDate flight date
-     * @return flying time response
-     *
+     * 算法：
+     * 1. 获取给定航班号和日期的季节性航班数据。
+     * 2. 如果没有季节性航班数据，返回一个 FlyingTimeResponse，其中 seasonal 设置为 false，history 设置为 false，flyingTime 设置为 0。
+     * 3. 如果有季节性航班数据，检索与季节性航班匹配的历史航班数据。
+     * 4. 过滤历史航班数据以获得合格的航班。
+     * 5. 如果合格航班的数量大于或等于 minHistoryFlight，则使用历史航班数据计算飞行时间。
+     * 6. 如果合格航班的数量小于 minHistoryFlight，则使用季节性航班数据确定飞行时间。
+     * 7. 返回计算出的 FlyingTimeResponse。
      */
     fun calculate(flightNumber: String, flightDate: Date): FlyingTimeResponse {
         log.info("Calculating flying time for $flightNumber on $flightDate")
