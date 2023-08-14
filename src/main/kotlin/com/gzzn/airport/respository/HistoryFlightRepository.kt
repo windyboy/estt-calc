087f6c71fb3c1a8d1db263714d64d7ff734caa4e
zhiqiang feng <fengzhq@it2000.com.cn>
2023-08-14 15:43:40 +0800

    format file and remove unused import

--- src/main/kotlin/com/gzzn/airport/respository/HistoryFlightRepository.kt
+++ src/main/kotlin/com/gzzn/airport/respository/HistoryFlightRepository.kt
@@ -5,12 +5,12 @@ import io.micronaut.data.annotation.Query
 import io.micronaut.data.jdbc.annotation.JdbcRepository
 import io.micronaut.data.model.query.builder.sql.Dialect
 import java.time.LocalDate
-import java.util.*
 
 
 @JdbcRepository(dialect = Dialect.ORACLE)
 interface HistoryFlightRepository {
-	@Query("""
+    @Query(
+        """
 select
   PRE_DEPT_DATETIME_ACTUAL pre_actual_time ,
   ACTUAL_DATETIME actual_time ,
@@ -21,10 +21,12 @@ from
 where
   ACTUAL_DATETIME is not null
   and PRE_DEPT_DATETIME_ACTUAL is not null
-  and  ARRI_OR_DEPT='A' and flight_number = :flightNumber
+  and  ARRI_OR_DEPT='A' 
+  and flight_number = :flightNumber
   and flight_date between  :startDate
-  and :endDate order by flight_date desc
+  and :endDate 
+  order by flight_date desc
   """
-	)
-	fun getArrivalFlight(flightNumber: String, startDate: LocalDate, endDate: LocalDate): List<HistoricalFlight>
+    )
+    fun getArrivalFlight(flightNumber: String, startDate: LocalDate, endDate: LocalDate): List<HistoricalFlight>
 }
