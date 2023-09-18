67dfbdf04eaaccd696c8f2dc966c78ea7f2e5636
zhiqiang feng <fengzhq@it2000.com.cn>
2023-09-18 14:51:22 +0800

    fix: update main test
    upgrade main server startup test script

--- src/test/kotlin/com/gzzn/airport/EsttCalcSpec.groovy
+++ src/test/kotlin/com/gzzn/airport/EsttCalcSpec.groovy
@@ -1,35 +1,19 @@
 package com.gzzn.airport
 
-import io.micronaut.context.ApplicationContext
-import io.micronaut.context.env.Environment
 import io.micronaut.runtime.EmbeddedApplication
-import jakarta.inject.Inject
-import spock.lang.AutoCleanup
-import spock.lang.Shared
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import spock.lang.Specification
+import jakarta.inject.Inject
 
-
+@MicronautTest
 class EsttCalcSpec extends Specification {
+
     @Inject
     EmbeddedApplication<?> application
-
-    @AutoCleanup
-    @Shared
-    ApplicationContext ctx = ApplicationContext.run()
 
     void 'test it works'() {
         expect:
         application.running
     }
 
-    void 'test env is detected'() {
-//        given:
-//        ApplicationContext ctx = ApplicationContext.run()
-
-        expect:
-        ctx.environment.getActiveNames().contains(Environment.TEST)
-
-//        cleanup:
-//        ctx.close()
-    }
 }
